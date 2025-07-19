65e607ef24c4761d6c9d05745908a25ee7d7bb73
AndrewQuijano <andrewquijano92@gmail.com>
2025-07-18 20:40:27 -0400

    Can now create Wheel files and setup pyproject.toml

--- setup.py
+++ setup.py
@@ -1,29 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
+import os
 
-# Function to read the requirements.txt file
-def parse_requirements(filename):
-    with open(filename, 'r') as file:
-        return [line.strip() for line in file if line.strip() and not line.startswith('#')]
 
-setup(
-    name='libinspector',
-    version='0.1.0',
-    author='NYU mLab',
-    author_email='your_email@example.com',
-    description='Library for core functionalities of IoT Inspector',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/nyu-mlab/inspector-core-library',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    package_data={
-        'libinspector': ['wireshark_oui_database.txt'],
-    },
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-    ],
-    python_requires='>=3.6',
-    install_requires=parse_requirements('requirements.txt'),  # Include requirements from requirements.txt
+if "PRETEND_VERSION" in os.environ:
+    version = os.environ["PRETEND_VERSION"]
+else:
+    from setuptools_scm import get_version
+    version = get_version(root='.',
+                          fallback_version="0.0.1",
+                          version_scheme="guess-next-dev",
+                          local_scheme="no-local-version")
+    version = version.partition(".dev")[0]
+    print(f"Using version: {version}")
+
+with open("README.md", "r") as fh:
+    description = fh.read()
+
+setuptools.setup(
+    long_description=description,
+    long_description_content_type="text/markdown",
+    version=version
 )
