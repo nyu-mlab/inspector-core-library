5a4a416bd81f354174832dbc9806ba5af7d8c6d3
AndrewQuijano <andrewquijano92@gmail.com>
2026-02-22 11:57:05 -0500

    Re-enable GeoIP, I'm pretty sure I remember the issue was Python 3.14

--- src/tests/test_geoip.py
+++ src/tests/test_geoip.py
@@ -3,22 +3,18 @@ import libinspector.privacy as privacy
 
 
 class TestGetCountryFromIpAddr(unittest.TestCase):
-    @unittest.skip("Skipping test to avoid external dependency")
     def test_private_ip(self):
         result = privacy.get_country_from_ip_addr('192.168.1.1')
         self.assertEqual(result, '(local network)')
 
-    @unittest.skip("Skipping test to avoid external dependency")
     def test_public_ip_found(self):
         result = privacy.get_country_from_ip_addr('8.8.8.8')
         self.assertEqual(result, 'United States')
 
-    @unittest.skip("Skipping test to avoid external dependency")
     def test_exception(self):
         result = privacy.get_country_from_ip_addr('8.8.4.4')
         self.assertEqual(result, 'United States')
 
-    @unittest.skip("Skipping test to avoid external dependency")
     def test_public_ip_not_found(self):
         result = privacy.get_country_from_ip_addr('1.2.3.4')
         self.assertEqual(result, 'Australia')
